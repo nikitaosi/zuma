4412ce43ca72336716d71ed126843ca7d8fc557f
Andrey <andreandr3evitch@yandex.ru>
2019-03-10 14:00:30 +0300

    Остановка части шаров при уничтожении одного из них

--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -12,6 +12,8 @@ export class MainScene extends Phaser.Scene {
   private graphics: Phaser.GameObjects.Graphics;
   private path: Phaser.Curves.Path;
   private follower :Phaser.GameObjects.Group;
+  private tween : Phaser.Tweens.Tween[];
+
 
   constructor() {
     super({
@@ -37,6 +39,7 @@ export class MainScene extends Phaser.Scene {
   {
     var vectors = [new Vector2( 164, 446, ),
                    ];
+    this.tween = [];
     this.graphics = this.add.graphics();
     this.path = new Phaser.Curves.Path(50,500);
    //this.path.splineTo(vectors);
@@ -49,17 +52,37 @@ export class MainScene extends Phaser.Scene {
     this.follower = this.add.group();
 
     for (let i = 0; i < 32; i++) {
-      let ball = this.follower.create(0,-50,'ball');
+      let ball = this.make.sprite({x:-50,key:'ball'});
+       ball.setData('order',i);
+      ball.setData('move',true);
+
+
+      this.follower.add(ball);
+
+      let rBall = <Phaser.GameObjects.Sprite>ball;
+      rBall.setData('scene',this);
+      ball.setInteractive();
+      ball.on('pointerdown',function (ev,ff,ff2,ff3,ff4) {
+        console.log("object order is: "+this.getData('order'));
+        console.log("object move is : "+this.getData('move'));
+        let scene = this.getData('scene');
+        scene.stopOthersBalls(this.getData('order'));
+
+        console.log(scene);
+
+
+
+      });
 
       ball.setData('vector',new Phaser.Math.Vector2());
-      this.tweens.add({
+      this.tween[i] = this.tweens.add({
         targets:ball,
         z:1,
         ease:'Sine.EaseInOut',
         duration:12000,
         // yoyo :true,
         //repeat: -1
-        delay:i*100
+        delay:i*400
       });
     }
 
@@ -68,20 +91,41 @@ export class MainScene extends Phaser.Scene {
 
   }
 
+  /**
+   * Останавливает шары идушие после уничноженного шара.
+   * @param count С какого по счету шара останавливается очередь
+   */
+  stopOthersBalls(count:integer):void
+  {
+      let ch = this.follower.getChildren();
+
+    for (let i = count; i < ch.length; i++) {
+      ch[i].setData('move',false);
+    }
+
+  }
+
   update(time: number, delta: number): void {
     this.graphics.clear();
     this.graphics.lineStyle(2, 0xffffff, 1);
     this.path.draw(this.graphics);
 
+
     this.follower.getChildren().forEach((function (ch) {
-      var vec = ch.getData('vector');
-      var sp = <Phaser.GameObjects.Sprite>ch;
-      var t = sp.z;
 
-     this.path.getPoint(t,vec);
-     sp.setPosition(vec.x,vec.y);
+      var allowMove = ch.getData('move');
 
-     sp.setDepth(ch.y);
+      if(allowMove)
+      {
+        var vec = ch.getData('vector');
+        var sp = <Phaser.GameObjects.Sprite>ch;
+        var t = sp.z;
+        this.path.getPoint(t,vec);
+        sp.setPosition(vec.x,vec.y);
+        sp.setDepth(ch.y);
+      }
+
+
 
 
 
